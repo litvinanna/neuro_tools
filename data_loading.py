e4d1367395c6078293bfec4b0e8cdb3b8c15cdb3
Litvin Anna cdr <pochtalionizm@gmail.com>
2019-04-23 10:28:06 +0300

    data-lodaing

--- data_loading.py
+++ data_loading.py
@@ -30,22 +30,50 @@ class Data():
     def __init__(self):
         self.train1 = None
 
-def get_data_sets(seq_np, length = 10, n_train = 100000, n_test=10000, genome_split = 0.1):
+def get_data_sets(seq_np, n_train = 100000, n_test=10000):
+
+    n_validate = n_train // 10
+    N = n_train + n_test + n_validate
+
     genome_l = len(seq_np)
+    ids = np.sort(np.random.choice(np.arange(start = 150, stop = genome_l - 150), N, replace=False))
+
+
+    test_start = np.random.choice(N - n_test - n_validate - 1)
+    test_end = test_start + n_test
+    validate_start = test_end
+    validate_end = validate_start + n_validate
+
+    test_ids = ids[test_start:test_end]
+    validate_ids = ids[validate_start:validate_end]
+    train_ids = np.concatenate((ids[0:test_start], ids[validate_end:genome_l])) 
     
-    train_ids = np.random.choice(int(genome_l * (1-genome_split)) - 300, n_train, replace=False) + 200
-    test_ids = np.random.choice(int(genome_l * genome_split) - 300, n_test, replace=False) + int(genome_l * (1-genome_split))
+    assert len(np.intersect1d(test_ids,     train_ids))     == 0
+    assert len(np.intersect1d(validate_ids, train_ids))     == 0
+    assert len(np.intersect1d(test_ids,     validate_ids))  == 0
 
 #    data = type('', (), {})()
     data = Data()
     
-    data.train1 = np.array([seq_np[x-length:x, ...] for x in train_ids])
-    data.test1 = np.array([seq_np[x-length:x, ...] for x in test_ids])
-
-    data.train2 = np.array([seq_np[x+1:x+length+1, ...] for x in train_ids])
-    data.test = np.array([seq_np[x+1:x+length+1, ...] for x in test_ids])
-        
-    data.train_ans = np.array([seq_np[x, ...]  for x in train_ids])
-    data.test_ans = np.array([seq_np[x, ...]  for x in test_ids])
+    data.train1    = np.array([seq_np[x-10:x, ...] for x in train_ids    ])
+    data.test1     = np.array([seq_np[x-10:x, ...] for x in test_ids     ])
+    data.validate1 = np.array([seq_np[x-10:x, ...] for x in validate_ids ])
+    
+    
+    data.train2    = np.array([seq_np[x+1:x+11, ...] for x in train_ids    ])
+    data.test2     = np.array([seq_np[x+1:x+11, ...] for x in test_ids     ])
+    data.validate2 = np.array([seq_np[x+1:x+11, ...] for x in validate_ids ])
+    
+    data.train3    = np.array([seq_np[x-100:x, ...] for x in train_ids    ])
+    data.test3     = np.array([seq_np[x-100:x, ...] for x in test_ids     ])
+    data.validate3 = np.array([seq_np[x-100:x, ...] for x in validate_ids ])
+    
+    data.train4    = np.array([seq_np[x+1:x+101, ...] for x in train_ids    ])
+    data.test4     = np.array([seq_np[x+1:x+101, ...] for x in test_ids     ])
+    data.validate4 = np.array([seq_np[x+1:x+101, ...] for x in validate_ids ])
+    
+    data.train_ans    = np.array([seq_np[x, ...]  for x in train_ids    ])
+    data.test_ans     = np.array([seq_np[x, ...]  for x in test_ids     ])
+    data.validate_ans = np.array([seq_np[x, ...]  for x in validate_ids ])
     
     return data
