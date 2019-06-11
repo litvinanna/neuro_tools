a1ee65b06479b4ae05edc63a2bd134a607f45b88
Litvin Anna cdr <pochtalionizm@gmail.com>
2019-06-11 11:53:27 +0300

    ...

--- tmp.sh
+++ tmp.sh
@@ -1,4 +1,1 @@
-python run_any_model_any_data_w_history.py  -t cnn -m 4 -e 51 -p 1 -s 0
-python run_any_model_any_data_w_history.py  -t cnn -m 5 -e 51 -p 1 -s 0
-python run_any_model_any_data_w_history.py  -t cnn -m 4 -e 51 -p 1 -s 0 --cds
-python run_any_model_any_data_w_history.py  -t cnn -m 5 -e 51 -p 1 -s 0 --cds
+python run_any_model_any_data_w_history.py -t cnn -m 21 -e 24 -p 1 --envtype 1 
